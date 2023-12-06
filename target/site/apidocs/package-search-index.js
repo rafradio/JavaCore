1c3e70c878d3f6116da7015d39e2cf1f64c4a2b1
Rafail Abdyushev <rafaelab@mail.ru>
2023-12-06 12:45:36 +0300

    target dit

---
+++ target/site/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.rafael.lessonjava"},{"l":"com.rafael.lessonjava.component"}];updateSearchResults();
---
+++ target/site/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.rafael.lessonjava","l":"Lessonjava"},{"p":"com.rafael.lessonjava.component","l":"MathClass"}];updateSearchResults();
